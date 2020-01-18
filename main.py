3e83a9ebbb5863170dee287572a5427b8077f7d3
Samuel Lando <samuel.lando@mail.concordia.ca>
2020-01-18 15:09:20 -0500

    Prepare main.py for notification

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from license_plate_publisher import *
 from payloads import *
 
+wanted=[]
 
 if __name__ == '__main__':
-    payload = get_plate_information(license_plate_publisher())
-    sendPayload(payload)
+    while True:
+        # TODO
+        if (there is an update):
+            wanted = getPayload()
+
+        payload = get_plate_information(license_plate_publisher())
+
+        if (payload["LicensePlate"] in wanted):
+            sendPayload(payload)
