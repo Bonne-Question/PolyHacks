f844ea7d63e445e4f4300f0ecaf66fe81023f393
sebastien <sebastien33goulet@gmail.com>
2020-01-18 14:02:57 -0500

    Part one receiving the images

--- main.py
+++ main.py
@@ -2,18 +2,14 @@ from azure.servicebus import ServiceBusService, Message, Topic, Rule, DEFAULT_RU
 
 
 bus_service_plate_publisher = ServiceBusService(
-    service_namespace="licenseplateread",
+    service_namespace="licenseplatepublisher",
     shared_access_key_name='ConsumeReads',
-    shared_access_key_value='VNcJZVQAVMazTAfrssP6Irzlg/pKwbwfnOqMXqROtCQ=',
-    account_key='r6GKjeC7ZaLkC46B'
+    shared_access_key_value='VNcJZVQAVMazTAfrssP6Irzlg/pKwbwfnOqMXqROtCQ='
 )
 
-bus_service_plate_publisher.create_subscription('licenseplateread', 'AllMessages')
+msg = bus_service_plate_publisher.receive_subscription_message('licenseplateread',
+                                                               'r6GKjeC7ZaLkC46B',
+                                                               peek_lock=False)
+print(msg.body)
 
-# topic_options = Topic()
-# topic_options.max_size_in_megabytes = '5120'
-# topic_options.default_message_time_to_live = 'PT1M'
-#
-# bus_service.create_topic('licenseplateread', topic_options)
-#
-# bus_service.create_subscription('licenseplateread', 'AllMessages')
+
